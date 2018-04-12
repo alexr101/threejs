79531e7d8a1d2006d18b35f1851bad66d2bc45b7
alexr101 <alex_r_101@hotmail.com>
2018-04-12 14:36:45 -0400

    add a lot of different things

--- js/app.js
+++ js/app.js
@@ -2,15 +2,31 @@ const example = (function() {
 	'use strict'; 
 	var scene = new THREE.Scene();
 	var renderer = window.WebGLRenderingContext ? new THREE.WebGLRenderer() : new THREE.canvasRenderer();
-	var light = new THREE.AmbientLight(0x4286f4);
+	var light = new THREE.DirectionalLight(0xffffff);
+	var ambientLight = new THREE.AmbientLight( 0xf4efdc ); 
 	var camera;
 	var boxes = [];
 	var sphere;
 
 	function initScene() {
 		renderer.setSize(window.innerWidth, window.innerHeight);
+		renderer.shadowMapEnabled = true;
+		renderer.shadowMapSoft = true;
+		
 		document.getElementById('web-gl-container').appendChild(renderer.domElement);
+
+		light.position.set(0, 2, 2);
+		light.target.position.set(0, 0, 0);
+		light.castShadow = true;
+		light.shadowDarkness = 1;
+		light.shadowCameraVisible = true;
+		light.shadow.mapSize.width = 1012;  
+		light.shadow.mapSize.height = 1012; 
+		light.shadow.camera.near = 1;    
+		light.shadow.camera.far = 1;  
+
 		scene.add(light);
+		scene.add( ambientLight );
 
 		camera  = new THREE.PerspectiveCamera(
 			35, 
@@ -18,27 +34,31 @@ const example = (function() {
 			1,
 			1000
 		);
-		camera.position.z = 100;
+		
+		var controls = new THREE.OrbitControls( camera );
+		camera.position.set( 0, 20, 100 );
+		controls.update();
+ 
 		scene.add(camera);
 
 		function addABunchOfBoxes(qty, size) {
 			for (let i = 0; i < qty; i++) {
 				var box = new THREE.Mesh(
 					new THREE.BoxGeometry(size, size, size),
-					new THREE.MeshBasicMaterial(0xffff00)
+					new THREE.MeshLambertMaterial(0xffff00)
 				);
 				box.name = 'boxy';
 				scene.add(box);
 				box.position.x =  i * (size*2) - 50;	
-				
+				box.castShadow = true;
+
 				boxes.push(box);
 			}
 		}
 
-		
-		addABunchOfBoxes(15, 3);
+		addABunchOfBoxes(20, 3);
 
-		var material = new THREE.MeshBasicMaterial({
+		var material = new THREE.MeshLambertMaterial({
 			color: 0xf4f4f4,
 			wireframe: true			
 		});
@@ -48,10 +68,22 @@ const example = (function() {
 			material
 		);
 		sphere.position.x = 30;
-		sphere.position.y = -10;
+		sphere.position.y = 10;
 		sphere.rotation.x = 45;
+		sphere.castShadow = true;
 		sphere.name = 'the sphere';
 		scene.add(sphere);
+
+		var geometry = new THREE.PlaneGeometry( 300, 300, 300 );
+		var material = new THREE.MeshLambertMaterial( {color: 0x555555, side: THREE.DoubleSide} );
+		var plane = new THREE.Mesh( geometry, material );
+		plane.receiveShadow = true;
+		plane.renderReverseSided  = true;
+		plane.position.y = -10;
+
+		scene.add( plane );
+		plane.rotateX( - Math.PI / 2);
+
 
 		render();
 	}
@@ -64,7 +96,16 @@ const example = (function() {
 		})
 
 		sphere.rotation.y += 0.01;
-		sphere.position.x -= 0.03;
+
+		if(sphere.goRight)
+			sphere.position.x += 0.5;
+		else
+			sphere.position.x -= .5
+
+		if(sphere.position.x < -30)
+			sphere.goRight = true;
+		if(sphere.position.x > 30)
+			sphere.goRight = false;
 		// if(sphere.position.x < window.innerWidth/2)
 		// 	sphere.position.x = 10;
 
