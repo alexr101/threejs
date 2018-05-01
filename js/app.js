48fbf65406034ec751242a7c61ee424b289b3617
alex padilla <alex.padilla@allthingsmedia.com>
2018-05-01 09:00:22 -0400

    working on dat.gui and real light scene

--- js/app.js
+++ js/app.js
@@ -3,25 +3,35 @@ const example = (function() {
 	var scene = new THREE.Scene();
 	var renderer = window.WebGLRenderingContext ? new THREE.WebGLRenderer() : new THREE.canvasRenderer();
 	var light = new THREE.PointLight(0xddc254, .8, 100); // color, intensity. distance
-	var ambientLight = new THREE.AmbientLight( 0xf4efdc, 1 ); // color, intensity 
+	var ambientLight = new THREE.AmbientLight( 0xf4efdc, 1 ); // color, intensity
+	var canvas = document.getElementById('web-gl-container');
 	var camera;
 	var boxes = [];
 	var sphere;
 	var USE_WIREFRAME = false;
+	var datGuiParams;
+	var bulbLuminousPowers;
+	var hemiLuminousIrradiances;
 
 	function initScene() {
 		renderer.setSize(window.innerWidth, window.innerHeight);
-		renderer.shadowMapEnabled = true;
+		renderer.shadowMap.enabled = true;
 		renderer.shadowMap.type = THREE.BasicShadowMap;
 		// renderer.shadowMapSoft = true;
 		
-		document.getElementById('web-gl-container').appendChild(renderer.domElement);
+		canvas.appendChild(renderer.domElement);
 
+		var lightGeometry = new THREE.SphereBufferGeometry( 1, 16, 8 );
+		var lightMat = new THREE.MeshStandardMaterial( {
+			emissive: 0xffffee,
+			emissiveIntensity: .2,
+			color: 0x000000
+		});
+		light.add( new THREE.Mesh( lightGeometry, lightMat ) );
 		light.position.set(-50, 10, 0);
 		light.castShadow = true;
 		light.shadow.camera.near = 0.1;    
 		light.shadow.camera.far = 250;  
-		light.power = 20*Math.PI;
 
 		scene.add(light);
 		scene.add( ambientLight );
@@ -33,7 +43,7 @@ const example = (function() {
 			1000
 		);
 		
-		var controls = new THREE.OrbitControls( camera );
+		var controls = new THREE.OrbitControls( camera, canvas );
 		camera.position.set( 0, 20, 100 );
 		controls.update();
  
@@ -59,26 +69,26 @@ const example = (function() {
 
 		addABunchOfBoxes(20, 3);
 
-		// var sphereMaterial = new THREE.MeshLambertMaterial({
-		// 	color: 0xf4f4f4,
-		// 	wireframe: USE_WIREFRAME			
-		// });
-		// sphere = new THREE.Mesh(
-		// 	new THREE.SphereGeometry(7, 7, 7),
-		// 	sphereMaterial
-		// );
-		// sphere.position.x = 30;
-		// sphere.position.y = 10;
-		// sphere.rotation.x = 45;
-		// sphere.castShadow = true;
-		// sphere.name = 'the sphere';
-		// sphere.animation = function(){
-		// 	if(this.goRight)
-		// 		this.position.x += 0.5;
-		// 	else
-		// 		this.position.x -= .5
-		// }
-		// scene.add(sphere);
+		var sphereMaterial = new THREE.MeshLambertMaterial({
+			color: 0xf4f4f4,
+			wireframe: USE_WIREFRAME			
+		});
+		sphere = new THREE.Mesh(
+			new THREE.SphereGeometry(7, 7, 7),
+			sphereMaterial
+		);
+		sphere.position.x = 30;
+		sphere.position.y = 10;
+		sphere.rotation.x = 45;
+		sphere.castShadow = true;
+		sphere.name = 'the sphere';
+		sphere.animation = function(){
+			if(this.goRight)
+				this.position.x += 0.5;
+			else
+				this.position.x -= .5
+		}
+		scene.add(sphere);
 
 		var planeGeometry = new THREE.PlaneGeometry( 300, 300, 300, 300 );
 		var planeMaterial = new THREE.MeshLambertMaterial( {
@@ -93,22 +103,68 @@ const example = (function() {
 		plane.rotateX( - Math.PI / 2);
 		scene.add( plane );
 
+		// DAT GUI PARAMS FOR LIGHT
+		// ref for lumens: http://www.power-sure.com/lumens.htm
+		bulbLuminousPowers = {
+			"110000 lm (1000W)": 110000,
+			"3500 lm (300W)": 3500,
+			"1700 lm (100W)": 1700,
+			"800 lm (60W)": 800,
+			"400 lm (40W)": 400,
+			"180 lm (25W)": 180,
+			"20 lm (4W)": 20,
+			"Off": 0
+		};
+		// ref for solar irradiances: https://en.wikipedia.org/wiki/Lux
+		hemiLuminousIrradiances = {
+			"0.0001 lx (Moonless Night)": 0.0001,
+			"0.002 lx (Night Airglow)": 0.002,
+			"0.5 lx (Full Moon)": 0.5,
+			"3.4 lx (City Twilight)": 3.4,
+			"50 lx (Living Room)": 50,
+			"100 lx (Very Overcast)": 100,
+			"350 lx (Office Room)": 350,
+			"400 lx (Sunrise/Sunset)": 400,
+			"1000 lx (Overcast)": 1000,
+			"18000 lx (Daylight)": 18000,
+			"50000 lx (Direct Sun)": 50000
+		};
+		datGuiParams = {
+			shadows: true,
+			exposure: 0.68,
+			bulbPower: Object.keys( bulbLuminousPowers )[ 1 ],
+			hemiIrradiance: Object.keys( hemiLuminousIrradiances )[0]
+		};
+		var gui = new dat.GUI();
+		gui.add( datGuiParams, 'hemiIrradiance', Object.keys( hemiLuminousIrradiances ) );
+		gui.add( datGuiParams, 'bulbPower', Object.keys( bulbLuminousPowers ) );
+		gui.add( datGuiParams, 'exposure', 0, 1 );
+		gui.add( datGuiParams, 'shadows' );
+		gui.open();
+
 		render();
 	}
 
 	function render(){
+		// BOX UPDATES
 		boxes.forEach(function(box){
 			box.rotation.x += 0.01;
 			box.rotation.y += 0.01;
 			box.rotation.z += 0.01;
 		})
 
-		// sphere.rotation.y += 0.01;
-		// sphere.animation();
-		// if(sphere.position.x < -30)
-		// 	sphere.goRight = true;
-		// if(sphere.position.x > 30)
-		// 	sphere.goRight = false;
+		// SPHERE  UPDATES
+		sphere.rotation.y += 0.01;
+		sphere.animation();
+		if(sphere.position.x < -30)
+			sphere.goRight = true;
+		if(sphere.position.x > 30)
+			sphere.goRight = false;
+
+		// DAT GUI UPDATES
+		light.power = bulbLuminousPowers[ datGuiParams.bulbPower ];
+
+
 
 		renderer.render(scene, camera);
 		requestAnimationFrame(render);
