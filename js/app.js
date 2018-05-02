83057889476cf9d42fbde0c1c5a0555a2af06120
alexr101 <alex_r_101@hotmail.com>
2018-05-01 21:52:37 -0400

    add second light, better lighting, better shadows

--- js/app.js
+++ js/app.js
@@ -2,8 +2,11 @@ const example = (function() {
 	'use strict'; 
 	var scene = new THREE.Scene();
 	var renderer = window.WebGLRenderingContext ? new THREE.WebGLRenderer() : new THREE.canvasRenderer();
-	var light = new THREE.PointLight(0xddc254, .8, 100); // color, intensity. distance
-	var ambientLight = new THREE.AmbientLight( 0xf4efdc, 1 ); // color, intensity
+	var light = new THREE.PointLight(0xf2e74b, 1, 1000, 2); // color, intensity. distance
+	var light2 = new THREE.PointLight(0x943df7, 1, 1000, 2); // color, intensity. distance
+
+	var ambientLight = new THREE.AmbientLight( 0xf4efdc, .2 ); // color, intensity
+	var hemiLight;
 	var canvas = document.getElementById('web-gl-container');
 	var camera;
 	var boxes = [];
@@ -16,38 +19,62 @@ const example = (function() {
 	function initScene() {
 		renderer.setSize(window.innerWidth, window.innerHeight);
 		renderer.shadowMap.enabled = true;
-		renderer.shadowMap.type = THREE.BasicShadowMap;
-		// renderer.shadowMapSoft = true;
-		
+		// next props: https://stackoverflow.com/questions/16752075/quality-of-three-js-shadow-in-chrome-macos
+		renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+		renderer.shadowMapSoft = true;
+		renderer.physicallyCorrectLights = true;
+		renderer.gammaInput = true;
+		renderer.gammaOutput = true;
+		renderer.toneMapping = THREE.ReinhardToneMapping;
+
 		canvas.appendChild(renderer.domElement);
 
 		var lightGeometry = new THREE.SphereBufferGeometry( 1, 16, 8 );
 		var lightMat = new THREE.MeshStandardMaterial( {
 			emissive: 0xffffee,
-			emissiveIntensity: .2,
+			emissiveIntensity: 1,
 			color: 0x000000
 		});
+		// next 2 props: https://stackoverflow.com/questions/16752075/quality-of-three-js-shadow-in-chrome-macos
+		light.shadowMapWidth = 2048; // default is 512
+		light.shadowMapHeight = 2048; // default is 512
 		light.add( new THREE.Mesh( lightGeometry, lightMat ) );
-		light.position.set(-50, 10, 0);
+		light.position.set(-150, 50, -100);
 		light.castShadow = true;
-		light.shadow.camera.near = 0.1;    
-		light.shadow.camera.far = 250;  
 
+		var lightGeometry2 = new THREE.SphereBufferGeometry( 1, 16, 8 );
+		var lightMat2 = new THREE.MeshStandardMaterial( {
+			emissive: 0xffffee,
+			emissiveIntensity: 1,
+			color: 0x000000
+		});
+		// next 2 props: https://stackoverflow.com/questions/16752075/quality-of-three-js-shadow-in-chrome-macos
+		light2.shadowMapWidth = 2048; // default is 512
+		light2.shadowMapHeight = 2048; // default is 512
+		light2.add( new THREE.Mesh( lightGeometry2, lightMat2 ) );
+		light2.position.set(150, 50, 100);
+		light2.castShadow = true;
+
+		hemiLight = new THREE.HemisphereLight( 0xddeeff, 0x0f0e0d, 0.02 );
+
+		scene.add( hemiLight );
 		scene.add(light);
+		scene.add(light2);
 		scene.add( ambientLight );
 
 		camera  = new THREE.PerspectiveCamera(
-			35, 
+			65, 
 			window.innerWidth/window.innerHeight,
 			1,
 			1000
 		);
-		
+		camera.position.set( -50, 130, 200 );
+		scene.add(camera);
+
 		var controls = new THREE.OrbitControls( camera, canvas );
-		camera.position.set( 0, 20, 100 );
 		controls.update();
  
-		scene.add(camera);
 
 		function addABunchOfBoxes(qty, size) {
 			for (let i = 0; i < qty; i++) {
@@ -60,25 +87,26 @@ const example = (function() {
 				);
 				box.name = 'boxy';
 				scene.add(box);
-				box.position.x =  i * (size*2) - 50;	
+				box.position.x =  i * (size*2) - 100;	
 				box.receiveShadow = true
 				box.castShadow = true;
 				boxes.push(box);
 			}
 		}
 
-		addABunchOfBoxes(20, 3);
+		addABunchOfBoxes(20, 10);
 
 		var sphereMaterial = new THREE.MeshLambertMaterial({
 			color: 0xf4f4f4,
+			roughness: 10,
 			wireframe: USE_WIREFRAME			
 		});
 		sphere = new THREE.Mesh(
-			new THREE.SphereGeometry(7, 7, 7),
+			new THREE.SphereGeometry(20, 20, 20),
 			sphereMaterial
 		);
 		sphere.position.x = 30;
-		sphere.position.y = 10;
+		sphere.position.y = 40;
 		sphere.rotation.x = 45;
 		sphere.castShadow = true;
 		sphere.name = 'the sphere';
@@ -90,10 +118,14 @@ const example = (function() {
 		}
 		scene.add(sphere);
 
-		var planeGeometry = new THREE.PlaneGeometry( 300, 300, 300, 300 );
+		var planeGeometry = new THREE.PlaneGeometry( 700, 300, 300, 300 );
 		var planeMaterial = new THREE.MeshLambertMaterial( {
 			color: 0x999999, 
+			metalness: 0.2,
+			reflectivity: 0.25,
 			side: THREE.DoubleSide,
+			roughness: 0.8,
+			bumpScale: 0.0005,
 			wireframe: USE_WIREFRAME
 		} );
 		var plane = new THREE.Mesh( planeGeometry, planeMaterial );
@@ -132,7 +164,7 @@ const example = (function() {
 		datGuiParams = {
 			shadows: true,
 			exposure: 0.68,
-			bulbPower: Object.keys( bulbLuminousPowers )[ 1 ],
+			bulbPower: Object.keys( bulbLuminousPowers )[ 6],
 			hemiIrradiance: Object.keys( hemiLuminousIrradiances )[0]
 		};
 		var gui = new dat.GUI();
@@ -162,8 +194,13 @@ const example = (function() {
 			sphere.goRight = false;
 
 		// DAT GUI UPDATES
+		renderer.toneMappingExposure = Math.pow( datGuiParams.exposure, 5.0 ); // to allow for very bright scenes.
+		renderer.shadowMap.enabled = datGuiParams.shadows;
+		light.castShadow = datGuiParams.shadows;
 		light.power = bulbLuminousPowers[ datGuiParams.bulbPower ];
-
+		light2.castShadow = datGuiParams.shadows;
+		light2.power = bulbLuminousPowers[ datGuiParams.bulbPower ];
+		hemiLight.intensity = hemiLuminousIrradiances[ datGuiParams.hemiIrradiance ];
 
 
 		renderer.render(scene, camera);
